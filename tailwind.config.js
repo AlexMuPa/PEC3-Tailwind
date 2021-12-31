4293fe52fe76c562613ca01e5ff3ab000d2d87a7
Alex Muñoz Palos <amunozpal@uoc.edu>
2021-12-31 16:50:49 +0100

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: ["./src/**/*.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    colors: {
+      'blue': '#054f91',
+      'white': '#FFF',
+      'second-blue': 'rgb(150, 193, 207)',
+      'slate': 'rgb(226 232 240)',
+      'black': 'rgb(0,0,0)'
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
